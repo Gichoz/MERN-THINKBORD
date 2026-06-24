d2de62a502819d1ef5e2b4c7f97064428d0ed2d3
Gichoz <kelvingichohi50@gmail.com>
2026-06-23 22:47:29 -0700

    Fix environment variable check for production base URL in axios configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE_ENV === "production" ? "/" : "http://localhost:5000/api";
+const BASE_URL = import.meta.env.MODE === "production" ? "/api" : "http://localhost:5000/api";
 const api = axios.create({
   baseURL: BASE_URL,
 });

bab1b12ebce43b56aa650533a020dbfc7091fe6f
Gichoz <kelvingichohi50@gmail.com>
2026-06-23 22:20:45 -0700

    Prepared & ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,50 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 app.use(express.json());
 // Apply rate limiter to all routes
 app.use(rateLimiter);
 
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use('/api/notes', notesRouter);
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Express 5 fix: use middleware, not app.get("/*")
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
-});
+}
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+  });
+};
+
+startServer();
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+//   });
+// });
 
 
 // import express from 'express';
